31663e9a35db2d20b33487023af918d886253c4c
Basanta Behera <basanta.behera@philips.com>
2018-09-29 23:28:11 +0530

    Initial version

---
+++ app/src/main/java/basanta/com/example/java/MainActivity.kt
@@ -0,0 +1,12 @@
+package basanta.com.example.java
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
